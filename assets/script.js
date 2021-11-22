a88e14f21976b5745da89936f10e9dc07ad0c012
monstermiles <miles.emmitt.moss@gmail.com>
2021-11-21 21:05:55 -0600

    made second api call for uv data

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,6 @@ function fetchData(){
   var cityName = inputField.value
   var apiKey = "836bcbaeb262be6529a98dc6c216e199"
   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey
-  
   fetch(requestUrl)
     .then(function(response) {
       return response.json()
@@ -16,11 +15,11 @@ function fetchData(){
       console.log(weatherData)
         //temperature  
         var temp = document.createElement("p")
-        temp.textContent = "Temperature: " + weatherData.main.temp;
+        temp.textContent = "Temperature: " + weatherData.main.temp + "F"
         weatherContainer.append(temp)
         //wind speed
         var windSpeed = document.createElement("p");
-        windSpeed.textContent = "Wind Speed: " + weatherData.wind.speed + " F"
+        windSpeed.textContent = "Wind Speed: " + weatherData.wind.speed;
         weatherContainer.append(windSpeed);
         //humidity
         var humidity = document.createElement("p");
@@ -30,8 +29,27 @@ function fetchData(){
         var uvIndex = document.createElement("p");
         // uvIndex.textContent = "UV Index: " + weatherData.main. + " %"
         weatherContainer.append(uvIndex);
+        
+        //grabbing lat and long to use in second API call
+        console.log(weatherData.coord.lat)
+        var lat = weatherData.coord.lat
+        var lon = weatherData.coord.lon
+
+        var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&appid=" + apiKey
+        fetch(requestUrl)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(uvWeatherData){
+                console.log(uvWeatherData)
+            }
+            )
+    
     
     })
+    
 }
 
 button.addEventListener("click", fetchData)
+
+

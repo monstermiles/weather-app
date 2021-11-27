2ee3975eace22f375d8aa7d0e7c85ec9f4271273
monstermiles <miles.emmitt.moss@gmail.com>
2021-11-26 19:20:22 -0600

    finished forecast cards, added styling

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,9 @@ var inputField = document.querySelector("#city")
 var button = document.querySelector("#get-weather")
 var weatherContainer = document.querySelector("#weather-container")
 var forecastContainer = document.querySelector("#forecast-container")
-
+var cityNameEl = document.querySelector("#city-name-el")
+var dateAndIcon = document.querySelector("#date-and-icon")
+var container = document.querySelector(".container")
 
 // var cityName = inputField.value
 //   var apiKey = "836bcbaeb262be6529a98dc6c216e199"
@@ -17,29 +19,34 @@ function megaFunction() {
         })
         .then(function (weatherData) {
             console.log(weatherData)
+            
             //add city name 
             var cityName = document.getElementById("city-name")
             cityName.textContent = weatherData.name
+            
             //add date and time 
             function getTimeAndDate() {
                 var timeAndDateEl = document.getElementById("time-and-date")
                 var timeAndDate = moment();
                 timeAndDateEl.textContent = timeAndDate.format("L" + ",  " + 'h' + ":" + "mm")
-                weatherContainer.append(timeAndDateEl)
             }
+            getTimeAndDate();
+
             //weather icon
             var iconEl = document.getElementById("icon-img")
             var icon = weatherData.weather[0].icon;
             iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
-            getTimeAndDate();
+            
             //temperature  
             var temp = document.createElement("p")
-            temp.textContent = "Temperature: " + weatherData.main.temp + " F"
+            temp.textContent = "Temperature: " + weatherData.main.temp + " °F"
             weatherContainer.append(temp)
+            
             //wind speed
             var windSpeed = document.createElement("p");
             windSpeed.textContent = "Wind Speed: " + weatherData.wind.speed;
             weatherContainer.append(windSpeed);
+            
             //humidity
             var humidity = document.createElement("p");
             humidity.textContent = "Humidity: " + weatherData.main.humidity + " %"
@@ -62,10 +69,10 @@ function megaFunction() {
 
                 //UV Index
                 var uvIndex = document.createElement("p");
-                uvIndex.textContent = "UV Index: " + UVData.current.uvi + " %"
+                uvIndex.textContent = "UV Index: " + UVData.current.uvi
                 weatherContainer.append(uvIndex);
                 var uvi = UVData.current.uvi
-                if (uvi > 0 && uvi <= 2) {
+                if (uvi >= 0 && uvi <= 2) {
                     uvIndex.classList.add("lowUVI")
                 }
                 if (uvi >= 2 && uvi < 5) {
@@ -92,6 +99,15 @@ function megaFunction() {
 
                     var forecast = forecastData.list
                     console.log(forecast)
+
+                    var forecastContainer = document.createElement("div")
+                    container.append(forecastContainer)
+                    forecastContainer.classList.add("forecast-container")
+                    forecastContainer.classList.add("row")
+
+                    var forecastText = document.createElement("h2")
+                    forecastText.textContent = "5 Day Forecast"
+                    forecastContainer.append(forecastText)
                     
                     //for loop to get data for each day's weather at 3pm
                     for (var i = 5; i < forecast.length; i = i + 8) {
@@ -105,6 +121,12 @@ function megaFunction() {
                         var forecastDate = document.createElement("h5")
                         forecastDate.textContent = moment.unix(forecast[i].dt).format("L")
                         forecastCard.append(forecastDate)
+                        
+                        //add icon 
+                        var forecastIconEl = document.createElement("img")
+                        var forecastIcon = forecast[i].weather[0].icon;
+                        forecastIconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png")
+                        forecastCard.append(forecastIconEl)
 
                         //add temp
                         var forecastTempK = forecast[i].main.temp
@@ -113,22 +135,30 @@ function megaFunction() {
                             }
                         var forecastTempF = convertTemp(forecastTempK)
                         var forecastTempEl = document.createElement("p")
-                        forecastTempEl.textContent = forecastTempF + " F"
+                        forecastTempEl.textContent = forecastTempF + " °F"
                         forecastCard.append(forecastTempEl)
 
+                        //add wind
+                        var forecastWindMS = forecast[i].wind.speed
+                        function convertWindSpeed(forecastWindMS) {
+                            return Math.floor(forecastWindMS * 2.237)
+                            }
+                        var forecastWindMph = convertWindSpeed(forecastWindMS)
+                        var forecastWindEl = document.createElement("p")
+                        forecastWindEl.textContent = "Wind" + forecastWindMph + " mph"
+                        forecastCard.append(forecastWindEl)
+                        
+                        //add humidity
+                        var forecastHumidity = document.createElement("p")
+                        forecastHumidity.textContent = forecast[i].main.humidity + " % humidity"
+                        forecastCard.append(forecastHumidity)
+                        
+
                     }
-
-
-
-
-                    })
-
-
+                })
             }
             fetchForecastData();
-
-        })
-    
+        })  
 }
 
 
